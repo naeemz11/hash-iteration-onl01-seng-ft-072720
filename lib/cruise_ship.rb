2b02e888ee4dcce39a286ce560f99e44a697061f
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 21:31:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,9 +5,9 @@
   suite_d: "Charlie Chaplin", 
  suite_e: "Crumpet the Elf"
  }
-
+winner= ""
 def select_winner(passengers)
-winner = ""
+
 
 passengers.each do |suite , name|
 if suite == :suite_a && name.start_with?("A")
