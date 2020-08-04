dc649f88632081e5c4c91f9232af8ed4390e1109
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 21:46:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,10 @@ def happy_birthday(birthday_kids)
       "Amanda" => 27
     }
 birthday_kids.each do |name, age|
-  puts "Happy Birthday #{name} You are now #{age} years old!" 
+  if age > 12
+  puts "Happy Birthday #{name}! You are now #{age} years old!" 
+else 
+  
 end 
 end
 
